6300e35d88c1ebcb656ca37e0a30a3e8ab0e2bd4
ramakrishna-m <rk884802@gmail.com>
2022-02-08 06:42:05 +0530

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,44 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class ActionPizzaOrder(Action):
+
+	def name(self) -> Text:
+		return "action_item_cost"
+
+
+
+	def submit(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict]:
+		pizza_size = tracker.get_slot("pizza_size")
+		pizza_type = tracker.get_slot("pizza_type")
+		pizza_amount = 100
+		
+		dispatcher.utter_message(text='Okay. Your order cost is {}'.format(pizza_amount))
+		return []
+
